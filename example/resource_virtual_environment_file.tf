ecf05f1cfbeb034816c9a9cb2a36ecf83d0e173c
Dan Petersen <dp@danitso.com>
2019-12-12 00:36:28 +0100

    Update example

--- example/resource_virtual_environment_file.tf
+++ example/resource_virtual_environment_file.tf
@@ -1,6 +1,27 @@
 resource "proxmox_virtual_environment_file" "alpine_template" {
   datastore_id = "${element(data.proxmox_virtual_environment_datastores.example.datastore_ids, index(data.proxmox_virtual_environment_datastores.example.datastore_ids, "local"))}"
+  file_name    = "alpine-3.10-default-amd64.tar.xz"
   node_name    = "${data.proxmox_virtual_environment_datastores.example.node_name}"
   source       = "${path.module}/assets/alpine-3.10-default_20190626_amd64.tar.xz"
   template     = true
+}
+
+output "resource_proxmox_virtual_environment_file_alpine_template_datastore_id" {
+  value = "${proxmox_virtual_environment_file.alpine_template.datastore_id}"
+}
+
+output "resource_proxmox_virtual_environment_file_alpine_template_file_name" {
+  value = "${proxmox_virtual_environment_file.alpine_template.file_name}"
+}
+
+output "resource_proxmox_virtual_environment_file_alpine_template_node_name" {
+  value = "${proxmox_virtual_environment_file.alpine_template.node_name}"
+}
+
+output "resource_proxmox_virtual_environment_file_alpine_template_source" {
+  value = "${proxmox_virtual_environment_file.alpine_template.source}"
+}
+
+output "resource_proxmox_virtual_environment_file_alpine_template_template" {
+  value = "${proxmox_virtual_environment_file.alpine_template.template}"
 }
