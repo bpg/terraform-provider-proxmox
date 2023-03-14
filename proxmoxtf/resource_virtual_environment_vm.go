d29fd97babab9a8f217b6ea0ffd89511c55624eb
Pavel Boldyrev <627562+bpg@users.noreply.github.com>
2023-03-13 23:45:08 -0400

    fix(vm): Fix `file_format` setting for new empty disks (#259)

--- proxmoxtf/resource_virtual_environment_vm.go
+++ proxmoxtf/resource_virtual_environment_vm.go
@@ -2442,6 +2442,7 @@ func resourceVirtualEnvironmentVMGetDiskDeviceObjects(
 
 		block := diskEntry.(map[string]interface{})
 		datastoreID, _ := block[mkResourceVirtualEnvironmentVMDiskDatastoreID].(string)
+		fileFormat, _ := block[mkResourceVirtualEnvironmentVMDiskFileFormat].(string)
 		fileID, _ := block[mkResourceVirtualEnvironmentVMDiskFileID].(string)
 		size, _ := block[mkResourceVirtualEnvironmentVMDiskSize].(int)
 		diskInterface, _ := block[mkResourceVirtualEnvironmentVMDiskInterface].(string)
@@ -2468,6 +2469,7 @@ func resourceVirtualEnvironmentVMGetDiskDeviceObjects(
 
 		diskDevice.ID = &datastoreID
 		diskDevice.Interface = &diskInterface
+		diskDevice.Format = &fileFormat
 		diskDevice.FileID = &fileID
 		sizeString := fmt.Sprintf("%dG", size)
 		diskDevice.Size = &sizeString
