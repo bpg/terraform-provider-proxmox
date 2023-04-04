ed3dfeae9907757f42c0cce63fe1f00a4e2ec0a2
Szczepan Wiśniowski <szczepan7700@gmail.com>
2023-04-05 01:55:48 +0200

    fix(provider): Deprecate `virtual_environment` block (#288)
    refactor(provider): Allow specifying attributes outside of virtual_environment block
    
    Also deprecate virtual_environment block, update docs and examples.
    
    Fixes #117
    Apparently CDKTF skips schemas without attributes, it has been fixed but it is available only in prerelease currently (https://github.com/hashicorp/terraform-cdk/pull/2736)
    
    Release-As: 0.17.0

--- example/main.tf
+++ example/main.tf
@@ -1,8 +1,6 @@
 provider "proxmox" {
-  virtual_environment {
-    endpoint = var.virtual_environment_endpoint
-    username = var.virtual_environment_username
-    password = var.virtual_environment_password
-    insecure = true
-  }
+  endpoint = var.virtual_environment_endpoint
+  username = var.virtual_environment_username
+  password = var.virtual_environment_password
+  insecure = true
 }
--- proxmoxtf/provider/schema.go
+++ proxmoxtf/provider/schema.go
@@ -7,123 +7,96 @@
 package provider
 
 import (
-	"errors"
-	"net/url"
+	"fmt"
 	"os"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
 )
 
 func createSchema() map[string]*schema.Schema {
+	providerSchema := nestedProviderSchema()
+	providerSchema[mkProviderVirtualEnvironment] = &schema.Schema{
+		Type:     schema.TypeList,
+		Optional: true,
+		Elem: &schema.Resource{
+			Schema: nestedProviderSchema(),
+		},
+		MaxItems:   1,
+		Deprecated: "Move attributes out of virtual_environment block",
+	}
+
+	return providerSchema
+}
+
+func nestedProviderSchema() map[string]*schema.Schema {
 	return map[string]*schema.Schema{
-		mkProviderVirtualEnvironment: {
-			Type:     schema.TypeList,
-			Optional: true,
-			Elem: &schema.Resource{
-				Schema: map[string]*schema.Schema{
-					mkProviderVirtualEnvironmentEndpoint: {
-						Type:        schema.TypeString,
-						Optional:    true,
-						Description: "The endpoint for the Proxmox Virtual Environment API",
-						DefaultFunc: schema.MultiEnvDefaultFunc(
-							[]string{"PROXMOX_VE_ENDPOINT", "PM_VE_ENDPOINT"},
-							dvProviderVirtualEnvironmentEndpoint,
-						),
-						ValidateFunc: func(v interface{}, k string) (warns []string, errs []error) {
-							value := v.(string)
-
-							if value == "" {
-								return []string{}, []error{
-									errors.New(
-										"you must specify an endpoint for the Proxmox Virtual Environment API (valid: https://host:port)",
-									),
-								}
-							}
-
-							_, err := url.ParseRequestURI(value)
-							if err != nil {
-								return []string{}, []error{
-									errors.New(
-										"you must specify a valid endpoint for the Proxmox Virtual Environment API (valid: https://host:port)",
-									),
-								}
-							}
-
-							return []string{}, []error{}
-						},
-					},
-					mkProviderVirtualEnvironmentInsecure: {
-						Type:        schema.TypeBool,
-						Optional:    true,
-						Description: "Whether to skip the TLS verification step",
-						DefaultFunc: func() (interface{}, error) {
-							for _, k := range []string{"PROXMOX_VE_INSECURE", "PM_VE_INSECURE"} {
-								v := os.Getenv(k)
-
-								if v == "true" || v == "1" {
-									return true, nil
-								}
-							}
-
-							return false, nil
-						},
-					},
-					mkProviderVirtualEnvironmentOTP: {
-						Type:        schema.TypeString,
-						Optional:    true,
-						Description: "The one-time password for the Proxmox Virtual Environment API",
-						DefaultFunc: schema.MultiEnvDefaultFunc(
-							[]string{"PROXMOX_VE_OTP", "PM_VE_OTP"},
-							dvProviderVirtualEnvironmentOTP,
-						),
-					},
-					mkProviderVirtualEnvironmentPassword: {
-						Type:        schema.TypeString,
-						Optional:    true,
-						Description: "The password for the Proxmox Virtual Environment API",
-						DefaultFunc: schema.MultiEnvDefaultFunc(
-							[]string{"PROXMOX_VE_PASSWORD", "PM_VE_PASSWORD"},
-							dvProviderVirtualEnvironmentPassword,
-						),
-						ValidateFunc: func(v interface{}, k string) (warns []string, errs []error) {
-							value := v.(string)
-
-							if value == "" {
-								return []string{}, []error{
-									errors.New(
-										"you must specify a password for the Proxmox Virtual Environment API",
-									),
-								}
-							}
-
-							return []string{}, []error{}
-						},
-					},
-					mkProviderVirtualEnvironmentUsername: {
-						Type:        schema.TypeString,
-						Optional:    true,
-						Description: "The username for the Proxmox Virtual Environment API",
-						DefaultFunc: schema.MultiEnvDefaultFunc(
-							[]string{"PROXMOX_VE_USERNAME", "PM_VE_USERNAME"},
-							dvProviderVirtualEnvironmentUsername,
-						),
-						ValidateFunc: func(v interface{}, k string) (warns []string, errs []error) {
-							value := v.(string)
-
-							if value == "" {
-								return []string{}, []error{
-									errors.New(
-										"you must specify a username for the Proxmox Virtual Environment API (valid: username@realm)",
-									),
-								}
-							}
-
-							return []string{}, []error{}
-						},
-					},
-				},
+		mkProviderEndpoint: {
+			Type:        schema.TypeString,
+			Optional:    true,
+			Description: "The endpoint for the Proxmox Virtual Environment API",
+			DefaultFunc: schema.MultiEnvDefaultFunc(
+				[]string{"PROXMOX_VE_ENDPOINT", "PM_VE_ENDPOINT"},
+				nil,
+			),
+			AtLeastOneOf: []string{
+				mkProviderEndpoint,
+				fmt.Sprintf("%s.0.%s", mkProviderVirtualEnvironment, mkProviderEndpoint),
 			},
-			MaxItems: 1,
+			ValidateFunc: validation.IsURLWithHTTPorHTTPS,
+		},
+		mkProviderInsecure: {
+			Type:        schema.TypeBool,
+			Optional:    true,
+			Description: "Whether to skip the TLS verification step",
+			DefaultFunc: func() (interface{}, error) {
+				for _, k := range []string{"PROXMOX_VE_INSECURE", "PM_VE_INSECURE"} {
+					v := os.Getenv(k)
+
+					if v == "true" || v == "1" {
+						return true, nil
+					}
+				}
+
+				return false, nil
+			},
+		},
+		mkProviderOTP: {
+			Type:        schema.TypeString,
+			Optional:    true,
+			Description: "The one-time password for the Proxmox Virtual Environment API",
+			DefaultFunc: schema.MultiEnvDefaultFunc(
+				[]string{"PROXMOX_VE_OTP", "PM_VE_OTP"},
+				dvProviderOTP,
+			),
+		},
+		mkProviderPassword: {
+			Type:        schema.TypeString,
+			Optional:    true,
+			Description: "The password for the Proxmox Virtual Environment API",
+			DefaultFunc: schema.MultiEnvDefaultFunc(
+				[]string{"PROXMOX_VE_PASSWORD", "PM_VE_PASSWORD"},
+				nil,
+			),
+			AtLeastOneOf: []string{
+				mkProviderPassword,
+				fmt.Sprintf("%s.0.%s", mkProviderVirtualEnvironment, mkProviderPassword),
+			},
+			ValidateFunc: validation.StringIsNotEmpty,
+		},
+		mkProviderUsername: {
+			Type:        schema.TypeString,
+			Optional:    true,
+			Description: "The username for the Proxmox Virtual Environment API",
+			DefaultFunc: schema.MultiEnvDefaultFunc(
+				[]string{"PROXMOX_VE_USERNAME", "PM_VE_USERNAME"},
+				nil,
+			),
+			AtLeastOneOf: []string{
+				mkProviderUsername,
+				fmt.Sprintf("%s.0.%s", mkProviderVirtualEnvironment, mkProviderUsername),
+			},
+			ValidateFunc: validation.StringIsNotEmpty,
 		},
 	}
 }
