401b39782f857382b30ab71b3e49a8ab44fbac48
Pavel Boldyrev <627562+bpg@users.noreply.github.com>
2023-04-07 21:58:37 -0400

    fix(file): "Permission denied" error when creating a file by a non-root user (#291)
    * fix(file): "Permission denied" error when creating a file by a non-root user
    
    * fix linter errors

--- proxmox/datastores_types.go
+++ proxmox/datastores_types.go
@@ -10,13 +10,28 @@ import (
 	"github.com/bpg/terraform-provider-proxmox/proxmox/types"
 )
 
-// VirtualEnvironmentDatastoreFileListResponseBody contains the body from a datastore content list response.
-type VirtualEnvironmentDatastoreFileListResponseBody struct {
-	Data []*VirtualEnvironmentDatastoreFileListResponseData `json:"data,omitempty"`
+// DatastoreGetResponseBody contains the body from a datastore get response.
+type DatastoreGetResponseBody struct {
+	Data *DatastoreGetResponseData `json:"data,omitempty"`
 }
 
-// VirtualEnvironmentDatastoreFileListResponseData contains the data from a datastore content list response.
-type VirtualEnvironmentDatastoreFileListResponseData struct {
+// DatastoreGetResponseData contains the data from a datastore get response.
+type DatastoreGetResponseData struct {
+	Content types.CustomCommaSeparatedList `json:"content,omitempty" url:"content,omitempty,comma"`
+	Digest  *string                        `json:"digest,omitempty"`
+	Path    *string                        `json:"path,omitempty"`
+	Shared  *types.CustomBool              `json:"shared,omitempty"`
+	Storage *string                        `json:"storage,omitempty"`
+	Type    *string                        `json:"type,omitempty"`
+}
+
+// DatastoreFileListResponseBody contains the body from a datastore content list response.
+type DatastoreFileListResponseBody struct {
+	Data []*DatastoreFileListResponseData `json:"data,omitempty"`
+}
+
+// DatastoreFileListResponseData contains the data from a datastore content list response.
+type DatastoreFileListResponseData struct {
 	ContentType    string  `json:"content"`
 	FileFormat     string  `json:"format"`
 	FileSize       int     `json:"size"`
@@ -26,13 +41,13 @@ type VirtualEnvironmentDatastoreFileListResponseData struct {
 	VolumeID       string  `json:"volid"`
 }
 
-// VirtualEnvironmentDatastoreGetStatusResponseBody contains the body from a datastore status get request.
-type VirtualEnvironmentDatastoreGetStatusResponseBody struct {
-	Data *VirtualEnvironmentDatastoreGetStatusResponseData `json:"data,omitempty"`
+// DatastoreGetStatusResponseBody contains the body from a datastore status get request.
+type DatastoreGetStatusResponseBody struct {
+	Data *DatastoreGetStatusResponseData `json:"data,omitempty"`
 }
 
-// VirtualEnvironmentDatastoreGetStatusResponseBody contains the data from a datastore status get request.
-type VirtualEnvironmentDatastoreGetStatusResponseData struct {
+// DatastoreGetStatusResponseData contains the data from a datastore status get request.
+type DatastoreGetStatusResponseData struct {
 	Active         *types.CustomBool               `json:"active,omitempty"`
 	AvailableBytes *int64                          `json:"avail,omitempty"`
 	Content        *types.CustomCommaSeparatedList `json:"content,omitempty" url:"content,omitempty,comma"`
@@ -43,8 +58,8 @@ type VirtualEnvironmentDatastoreGetStatusResponseData struct {
 	UsedBytes      *int64                          `json:"used,omitempty"`
 }
 
-// VirtualEnvironmentDatastoreListRequestBody contains the body for a datastore list request.
-type VirtualEnvironmentDatastoreListRequestBody struct {
+// DatastoreListRequestBody contains the body for a datastore list request.
+type DatastoreListRequestBody struct {
 	ContentTypes types.CustomCommaSeparatedList `json:"content,omitempty" url:"content,omitempty,comma"`
 	Enabled      *types.CustomBool              `json:"enabled,omitempty" url:"enabled,omitempty,int"`
 	Format       *types.CustomBool              `json:"format,omitempty"  url:"format,omitempty,int"`
@@ -52,13 +67,13 @@ type VirtualEnvironmentDatastoreListRequestBody struct {
 	Target       *string                        `json:"target,omitempty"  url:"target,omitempty"`
 }
 
-// VirtualEnvironmentDatastoreListResponseBody contains the body from a datastore list response.
-type VirtualEnvironmentDatastoreListResponseBody struct {
-	Data []*VirtualEnvironmentDatastoreListResponseData `json:"data,omitempty"`
+// DatastoreListResponseBody contains the body from a datastore list response.
+type DatastoreListResponseBody struct {
+	Data []*DatastoreListResponseData `json:"data,omitempty"`
 }
 
-// VirtualEnvironmentDatastoreListResponseData contains the data from a datastore list response.
-type VirtualEnvironmentDatastoreListResponseData struct {
+// DatastoreListResponseData contains the data from a datastore list response.
+type DatastoreListResponseData struct {
 	Active              *types.CustomBool               `json:"active,omitempty"`
 	ContentTypes        *types.CustomCommaSeparatedList `json:"content,omitempty"`
 	Enabled             *types.CustomBool               `json:"enabled,omitempty"`
@@ -71,8 +86,8 @@ type VirtualEnvironmentDatastoreListResponseData struct {
 	Type                string                          `json:"type,omitempty"`
 }
 
-// VirtualEnvironmentDatastoreUploadRequestBody contains the body for a datastore upload request.
-type VirtualEnvironmentDatastoreUploadRequestBody struct {
+// DatastoreUploadRequestBody contains the body for a datastore upload request.
+type DatastoreUploadRequestBody struct {
 	ContentType string    `json:"content,omitempty"`
 	DatastoreID string    `json:"storage,omitempty"`
 	FileName    string    `json:"filename,omitempty"`
@@ -80,7 +95,7 @@ type VirtualEnvironmentDatastoreUploadRequestBody struct {
 	NodeName    string    `json:"node,omitempty"`
 }
 
-// VirtualEnvironmentDatastoreUploadResponseBody contains the body from a datastore upload response.
-type VirtualEnvironmentDatastoreUploadResponseBody struct {
+// DatastoreUploadResponseBody contains the body from a datastore upload response.
+type DatastoreUploadResponseBody struct {
 	UploadID *string `json:"data,omitempty"`
 }
