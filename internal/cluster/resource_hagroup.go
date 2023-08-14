1cc7e6cd169c038e177b46f1c418372011c46593
Emmanuel BENOÎT <tseeker@nocternity.net>
2023-08-14 11:31:18 +0200

    feat(ha): schema definition for the HA group resource

--- internal/cluster/resource_hagroup.go
+++ internal/cluster/resource_hagroup.go
@@ -9,11 +9,17 @@ package cluster
 import (
 	"context"
 	"fmt"
+	"regexp"
 
+	"github.com/bpg/terraform-provider-proxmox/internal/tffwk"
 	"github.com/bpg/terraform-provider-proxmox/proxmox"
 	hagroups "github.com/bpg/terraform-provider-proxmox/proxmox/cluster/ha/groups"
+	"github.com/hashicorp/terraform-plugin-framework-validators/int64validator"
+	"github.com/hashicorp/terraform-plugin-framework-validators/mapvalidator"
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 )
 
@@ -55,7 +61,52 @@ func (r *hagroupResource) Schema(
 	_ resource.SchemaRequest,
 	resp *resource.SchemaResponse,
 ) {
-	resp.Schema = schema.Schema{}
+	resp.Schema = schema.Schema{
+		Description: "Manages a High Availability group in a Proxmox VE cluster.",
+		Attributes: map[string]schema.Attribute{
+			"id": tffwk.IDAttribute(),
+			"group": schema.StringAttribute{
+				Description: "The identifier of the High Availability group to manage.",
+				Required:    true,
+				Validators: []validator.String{
+					stringvalidator.RegexMatches(
+						regexp.MustCompile(`^[a-zA-Z][a-zA-Z0-9\-_\.]*[a-zA-Z0-9]$`),
+						"must start with a letter, end with a letter or number, be composed of "+
+							"letters, numbers, '-', '_' and '.', and must be at least 2 characters long.",
+					),
+				},
+			},
+			"comment": schema.StringAttribute{
+				Description: "The comment associated with this group",
+				Optional:    true,
+			},
+			"members": schema.MapAttribute{
+				Description: "The member nodes for this group, associated with their priority or to null if no priority is set.",
+				Required:    true,
+				ElementType: types.Int64Type,
+				Validators: []validator.Map{
+					mapvalidator.SizeAtLeast(1),
+					mapvalidator.KeysAre(
+						stringvalidator.RegexMatches(
+							regexp.MustCompile(`^[a-zA-Z0-9]([a-zA-Z0-9\-]*[a-zA-Z0-9])?$`),
+							"must be a valid Proxmox node name",
+						),
+					),
+					mapvalidator.ValueInt64sAre(int64validator.AtLeast(0)),
+				},
+			},
+			"no_failback": schema.BoolAttribute{
+				Description: "A flag that indicates that failing back to a higher priority node is disabled for this HA " +
+					"group. Defaults to `false`.",
+				Optional: true,
+			},
+			"restricted": schema.BoolAttribute{
+				Description: "A flag that indicates that other nodes may not be used to run resources associated to this HA " +
+					"group. Defaults to `false`.",
+				Optional: true,
+			},
+		},
+	}
 }
 
 // Configure accesses the provider-configured Proxmox API client on behalf of the resource.
