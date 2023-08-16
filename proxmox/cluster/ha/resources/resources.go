d469d83b55b4a67ca7e45c9b440c0a679a4df810
Emmanuel BENOÎT <tseeker@nocternity.net>
2023-08-16 11:43:50 +0200

    feat(api): HA resource creation, update and deletion

--- proxmox/cluster/ha/resources/resources.go
+++ proxmox/cluster/ha/resources/resources.go
@@ -60,3 +60,33 @@ func (c *Client) Get(ctx context.Context, id types.HAResourceID) (*HAResourceGet
 
 	return resBody.Data, nil
 }
+
+// Create creates a new HA resource.
+func (c *Client) Create(ctx context.Context, data *HAResourceCreateRequestBody) error {
+	err := c.DoRequest(ctx, http.MethodPost, c.ExpandPath(""), data, nil)
+	if err != nil {
+		return fmt.Errorf("error creating HA resource: %w", err)
+	}
+
+	return nil
+}
+
+// Update updates an existing HA resource.
+func (c *Client) Update(ctx context.Context, id types.HAResourceID, data *HAResourceUpdateRequestBody) error {
+	err := c.DoRequest(ctx, http.MethodPut, c.ExpandPath(url.PathEscape(id.String())), data, nil)
+	if err != nil {
+		return fmt.Errorf("error updating HA resource %v: %w", id, err)
+	}
+
+	return nil
+}
+
+// Delete deletes a HA resource.
+func (c *Client) Delete(ctx context.Context, id types.HAResourceID) error {
+	err := c.DoRequest(ctx, http.MethodDelete, c.ExpandPath(url.PathEscape(id.String())), nil, nil)
+	if err != nil {
+		return fmt.Errorf("error deleting HA resource %v: %w", id, err)
+	}
+
+	return nil
+}
