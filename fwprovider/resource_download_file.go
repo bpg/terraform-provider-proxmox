3e35e1ca5955dd6863962807c5ccb2fd5141a9ad
Rafał Safin <rafal.safin@rafsaf.pl>
2023-12-28 02:10:00 +0100

    fix(storage): typo in resource download file error
    Signed-off-by: Rafał Safin <rafal.safin@rafsaf.pl>

--- fwprovider/resource_download_file.go
+++ fwprovider/resource_download_file.go
@@ -303,8 +303,8 @@ func (r *downloadFileResource) Create(
 	if err != nil {
 		if strings.Contains(err.Error(), "refusing to override existing file") {
 			resp.Diagnostics.AddError(
-				"File already exists in a datastore, it was createad outside of Terraform "+
-					"or is managed by another terraform resource.",
+				"File already exists in a datastore, it was created outside of Terraform "+
+					"or is managed by another resource.",
 				fmt.Sprintf(
 					"File already exists in a datastore: `%s`, "+
 						"error: %s", filename, err.Error()),
