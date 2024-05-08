e0d2c09cf538fc3597ad27b2ac1fd8a249aa83c4
Pavel Boldyrev <627562+bpg@users.noreply.github.com>
2024-05-07 22:28:29 -0400

    add examples
    Signed-off-by: Pavel Boldyrev <627562+bpg@users.noreply.github.com>

---
+++ examples/resources/proxmox_virtual_environment_user_token/import.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env sh
+#Tokens can be imported using they identifiers in format `user_id!token_name` format, e.g.:
+terraform import proxmox_virtual_environment_user_token.token1 user@pve!token1
