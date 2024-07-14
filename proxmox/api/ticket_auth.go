59de21547a40096d636f2f9784da6aae77077fbe
vanillaSprinkles <vanillaSprinkles@users.noreply.github.com>
2024-07-14 00:30:59 -0700

    feat(provider): add support for pre(external) auth'd session tokens
    adds provider config inputs:
      - env vars: PROXMOX_VE_AUTH_PAYLOAD; PROXMOX_VE_AUTH_TICKET with PROXMOX_VE_CSRF_PREVENTION_TOKEN
      - provider-config: auth_payload; auth_ticket with csrf_prevention_token
    
    Signed-off-by: vanillaSprinkles <vanillaSprinkles@users.noreply.github.com>

--- proxmox/api/ticket_auth.go
+++ proxmox/api/ticket_auth.go
@@ -14,6 +14,8 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"strconv"
+	"strings"
 	"sync"
 
 	"github.com/hashicorp/terraform-plugin-log/tflog"
@@ -29,8 +31,86 @@ type ticketAuthenticator struct {
 	mu sync.Mutex
 }
 
+// custom bool to handle bools of: int, text, bool // ref: https://stackoverflow.com/questions/4278430/convert-string-to-integer-type-in-go
+func (bit *cBool) UnmarshalJSON(inBits []byte) error {
+	inString := strings.ToLower(string(inBits))
+
+	inInt, err := strconv.Atoi(inString)
+	if err != nil {
+		return fmt.Errorf("failed injesting bool: %s", inString)
+	}
+
+	switch {
+	case inInt > 0 || inString == "true":
+		*bit = true
+	case inInt == 0 || inString == "false":
+		*bit = false
+	default:
+		return fmt.Errorf("cannot unmarshal boolean: %s", inString)
+	}
+
+	return nil
+}
+
 // NewTicketAuthenticator returns a new ticket authenticator.
+// precedence:  AuthPayload > AuthTicket & CSRFPreventionToken > OTP > user+pass // aka:  pre-auth > OTP > user+pass
 func NewTicketAuthenticator(conn *Connection, creds *Credentials) (Authenticator, error) {
+	if creds.AuthPayload != "" {
+		resBody := AuthenticationResponseBody{}
+
+		err := json.Unmarshal([]byte(creds.AuthPayload), &resBody)
+		if err != nil {
+			return nil, fmt.Errorf("failed to decode auth_payload json, %w", err)
+		}
+
+		//nolint:lll
+		if resBody.Data == nil || (resBody.Data.Ticket == nil || *resBody.Data.Ticket == "") || (resBody.Data.CSRFPreventionToken == nil || *resBody.Data.CSRFPreventionToken == "") {
+			// not enforcing Username, nor capabilities in authPayload
+			return nil, fmt.Errorf("external auth_payload is invalid; eg: '{\"data\":{\"username\":\"USER@REALM\",\"ticket\":\"PVE:USER@REALM:UNIQUEID::BASE64BLOB\",\"CSRFPreventionToken\":\"UNIQUEID:BLOB\",\"cap\":{}}}}'")
+		}
+
+		if resBody.Data.NeedTFA != nil && *(resBody.Data.NeedTFA) {
+			return nil, fmt.Errorf("external auth_payload contains key NeedTFA and requires two-factor authentication")
+		}
+
+		creds.Username = resBody.Data.Username                              // todo: exclude line? - is creds.Username needed/used anywhere other than new-auth reqs?
+		if creds.Username != "" && !strings.Contains(creds.Username, "@") { // todo: improve this vs copy-pasta from credentials.go
+			return nil, errors.New(
+				"make sure the username for the Proxmox Virtual Environment API ends in '@pve or @pam'",
+			)
+		}
+
+		return &ticketAuthenticator{
+			conn:     conn,
+			authData: resBody.Data,
+		}, nil
+	}
+
+	if creds.AuthTicket != "" && creds.CSRFPreventionToken != "" {
+		ard := &AuthenticationResponseData{}
+		ard.Ticket = &(creds.AuthTicket)
+		ard.CSRFPreventionToken = &(creds.CSRFPreventionToken)
+
+		authTicketSplits := strings.Split(creds.AuthTicket, ":")
+
+		if len(authTicketSplits) > 3 {
+			creds.Username = strings.Split(creds.AuthTicket, ":")[1] // todo: exclude line? - is creds.Username needed/used anywhere other than new-auth reqs?
+		} else {
+			return nil, errors.New("auth_ticket is set to an invalid value")
+		}
+
+		if creds.Username != "" && !strings.Contains(creds.Username, "@") { // todo: improve this vs copy-pasta from credentials.go
+			return nil, errors.New(
+				"make sure the username for the Proxmox Virtual Environment API ends in '@pve or @pam'",
+			)
+		}
+
+		return &ticketAuthenticator{
+			conn:     conn,
+			authData: ard,
+		}, nil
+	}
+
 	authRequest := fmt.Sprintf(
 		"username=%s&password=%s",
 		url.QueryEscape(creds.Username),
@@ -38,6 +118,12 @@ func NewTicketAuthenticator(conn *Connection, creds *Credentials) (Authenticator
 	)
 
 	// OTP is optional, and probably doesn't make much sense for most provider users.
+	//   TOTP uses 2x requests; one with payloads `username=` and `password=`,
+	//     (this returns a payload including: 'NeedTFA=1')
+	//   followed by a 2nd request with payloads:
+	//     `username=`, `tfa-challenge=<firsts response ticket>`, `password=totp:######`,
+	//   and header: `CSRFPreventionToken: <first response CSRF>`
+	//   Ticket generated lasts for ~2hours (to verify)
 	if creds.OTP != nil {
 		authRequest = fmt.Sprintf("%s&otp=%s", authRequest, url.QueryEscape(*creds.OTP))
 	}
