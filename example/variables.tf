abb0be4684fa21dd5e7b600f238348c509b6c9af
Pavel Boldyrev <627562+bpg@users.noreply.github.com>
2024-08-19 14:20:05 -0400

    chore(example): update links to container images, use faster mirror (#1497)
    Signed-off-by: Pavel Boldyrev <627562+bpg@users.noreply.github.com>

--- example/variables.tf
+++ example/variables.tf
@@ -19,14 +19,14 @@ variable "latest_debian_12_bookworm_qcow2_img_url" {
   default     = "https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-generic-amd64.qcow2"
 }
 
-variable "release_20240416_ubuntu_22_jammy_lxc_img_url" {
+variable "release_20240725_ubuntu_24_noble_lxc_img_url" {
   type        = string
-  description = "The URL for the Ubuntu 22.04 LXC image"
-  default     = "https://cloud-images.ubuntu.com/releases/22.04/release-20240416/ubuntu-22.04-server-cloudimg-amd64-root.tar.xz"
+  description = "The URL for the Ubuntu 24.04 LXC image"
+  default     = "https://mirrors.servercentral.com/ubuntu-cloud-images/releases/24.04/release-20240725/ubuntu-24.04-server-cloudimg-amd64-root.tar.xz"
 }
 
-variable "release_20240416_ubuntu_22_jammy_lxc_img_checksum" {
+variable "release_20240725_ubuntu_24_noble_lxc_img_checksum" {
   type        = string
-  description = "The checksum for the Ubuntu 22.04 LXC image"
-  default     = "a362bf415ad2eae4854deda6237894a96178a2edbbd5a1956d6c55c5837a80d3"
+  description = "The checksum for the Ubuntu 24.04 LXC image"
+  default     = "d767d38cb25b2c25d84edc31a80dd1c29a8c922b74188b0e14768b2b2fb6df8e"
 }
