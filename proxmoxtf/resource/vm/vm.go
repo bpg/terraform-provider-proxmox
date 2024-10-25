bpg/terraform-provider-proxmox

commit 90c50fcb471c66448652f089039ed8cef9a3bbc7

fix(vm): do not reboot at disk resize (#1580)
Signed-off-by: Pavel Boldyrev <627562+bpg@users.noreply.github.com>

proxmoxtf/resource/vm/vm.go
@@ -5523,12 +5523,19 @@ func vmUpdateDiskLocationAndSize(
 					)
 				} else {
 					return diag.Errorf(
-						"Cannot resize %s:%s in VM %d configuration, it is not owned by this VM!",
+						"Cannot resize %s:%s in VM %d, it is not owned by this VM!",
 						*oldDisk.DatastoreID,
 						*oldDisk.PathInDatastore(),
 						vmID,
 					)
 				}
+			} else {
+				return diag.Errorf(
+					"Cannot shrink %s:%s in VM %d, it is not supported!",
+					*oldDisk.DatastoreID,
+					*oldDisk.PathInDatastore(),
+					vmID,
+				)
 			}
 		}
 

proxmox/helpers/ptr/ptr.go
@@ -19,3 +19,16 @@ func Or[T any](p *T, or T) T {
 
 	return or
 }
+
+// Eq compares two pointers and returns true if they are equal.
+func Eq[T comparable](a, b *T) bool {
+	if a == nil && b == nil {
+		return true
+	}
+
+	if a == nil || b == nil {
+		return false
+	}
+
+	return *a == *b
+}

proxmoxtf/resource/vm/disk/disk.go
@@ -20,6 +20,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
 	"github.com/bpg/terraform-provider-proxmox/proxmox"
+	"github.com/bpg/terraform-provider-proxmox/proxmox/helpers/ptr"
 	"github.com/bpg/terraform-provider-proxmox/proxmox/nodes/vms"
 	"github.com/bpg/terraform-provider-proxmox/proxmox/ssh"
 	"github.com/bpg/terraform-provider-proxmox/proxmox/types"
@@ -602,7 +603,7 @@ func Update(
 				continue
 			}
 
-			if tmp.AIO != disk.AIO {
+			if !ptr.Eq(tmp.AIO, disk.AIO) {
 				rebootRequired = true
 				tmp.AIO = disk.AIO
 			}

commit e1d03e3c12de9746d583eb59da2a2fbbe3777294

fix(vm): incorrect detection of disk resize
Regression after #1580, the provider incorrectly detects that disk has been resized when other disk attributes change.

Signed-off-by: Pavel Boldyrev <627562+bpg@users.noreply.github.com>

proxmoxtf/resource/vm/vm.go
@@ -5512,30 +5512,32 @@ func vmUpdateDiskLocationAndSize(
 				}
 			}
 
-			if *oldDisk.Size < *diskNewEntries[oldIface].Size {
-				if oldDisk.IsOwnedBy(vmID) {
-					diskResizeBodies = append(
-						diskResizeBodies,
-						&vms.ResizeDiskRequestBody{
-							Disk: oldIface,
-							Size: *diskNewEntries[oldIface].Size,
-						},
-					)
+			if *oldDisk.Size != *diskNewEntries[oldIface].Size {
+				if *oldDisk.Size < *diskNewEntries[oldIface].Size {
+					if oldDisk.IsOwnedBy(vmID) {
+						diskResizeBodies = append(
+							diskResizeBodies,
+							&vms.ResizeDiskRequestBody{
+								Disk: oldIface,
+								Size: *diskNewEntries[oldIface].Size,
+							},
+						)
+					} else {
+						return diag.Errorf(
+							"Cannot resize %s:%s in VM %d, it is not owned by this VM!",
+							*oldDisk.DatastoreID,
+							*oldDisk.PathInDatastore(),
+							vmID,
+						)
+					}
 				} else {
 					return diag.Errorf(
-						"Cannot resize %s:%s in VM %d, it is not owned by this VM!",
+						"Cannot shrink %s:%s in VM %d, it is not supported!",
 						*oldDisk.DatastoreID,
 						*oldDisk.PathInDatastore(),
 						vmID,
 					)
 				}
-			} else {
-				return diag.Errorf(
-					"Cannot shrink %s:%s in VM %d, it is not supported!",
-					*oldDisk.DatastoreID,
-					*oldDisk.PathInDatastore(),
-					vmID,
-				)
 			}
 		}
 