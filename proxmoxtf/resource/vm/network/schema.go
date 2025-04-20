5974d8b3a37835735c182c901ab873ab3b3c71eb
Pavel Boldyrev <627562+bpg@users.noreply.github.com>
2025-04-19 22:52:21 -0400

    fix(vm): cloned VM may be recreated/updated on re-apply (#1914)
    Signed-off-by: Pavel Boldyrev <627562+bpg@users.noreply.github.com>

--- proxmoxtf/resource/vm/network/schema.go
+++ proxmoxtf/resource/vm/network/schema.go
@@ -75,9 +75,7 @@ func Schema() map[string]*schema.Schema {
 			Type:        schema.TypeList,
 			Description: "The network devices",
 			Optional:    true,
-			DefaultFunc: func() (interface{}, error) {
-				return make([]interface{}, 1), nil
-			},
+			Computed:    true,
 			Elem: &schema.Resource{
 				Schema: map[string]*schema.Schema{
 					mkNetworkDeviceBridge: {
