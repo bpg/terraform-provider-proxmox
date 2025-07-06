b1ee614b1e8c5676d232f93242dc3333f2c2f466
Pavel Boldyrev <627562+bpg@users.noreply.github.com>
2025-07-06 11:45:33 -0400

    chore: re-gen docs
    Signed-off-by: Pavel Boldyrev <627562+bpg@users.noreply.github.com>

--- examples/guides/cloud-init/custom/main.tf
+++ examples/guides/cloud-init/custom/main.tf
@@ -43,7 +43,7 @@ resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_image" {
   content_type = "import"
   datastore_id = "local"
   node_name    = "pve"
-  url = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
+  url          = "https://cloud-images.ubuntu.com/jammy/current/jammy-server-cloudimg-amd64.img"
   # need to rename the file to *.qcow2 to indicate the actual file format for import
   file_name = "jammy-server-cloudimg-amd64.qcow2"
 }
