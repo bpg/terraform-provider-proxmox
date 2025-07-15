c1016a96eb366bde15b9ec6f385788d67194bc1d
Pavel Boldyrev <627562+bpg@users.noreply.github.com>
2025-07-14 21:43:36 -0400

    add zones api client
    Signed-off-by: Pavel Boldyrev <627562+bpg@users.noreply.github.com>

---
+++ proxmox/cluster/sdn/zones/doc.go
@@ -0,0 +1,11 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+// Package `zones` provides types and structures for managing Proxmox VE Software Defined Network (SDN) zones.
+// SDN zones are logical network segments that can be configured with different network types including
+// VLAN, QinQ, VXLAN, and EVPN. This package contains the data structures used for creating, updating,
+// and managing SDN zones through the Proxmox API.
+package zones
