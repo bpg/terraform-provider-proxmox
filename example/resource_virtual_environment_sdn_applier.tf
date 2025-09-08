c275e031ffdd9b8fdd46c09d1563be12c713541b
Pavel Boldyrev <627562+bpg@users.noreply.github.com>
2025-09-07 22:07:48 -0400

    chore(docs): update sdn_applier example
    Signed-off-by: Pavel Boldyrev <627562+bpg@users.noreply.github.com>

--- example/resource_virtual_environment_sdn_applier.tf
+++ example/resource_virtual_environment_sdn_applier.tf
@@ -1,16 +1,24 @@
 resource "proxmox_virtual_environment_sdn_zone_simple" "test_zone_1" {
   id = "tZone1"
-  nodes= [data.proxmox_virtual_environment_nodes.example.names]
+  nodes= data.proxmox_virtual_environment_nodes.example.names
   mtu = 1496
+
+  depends_on = [
+    proxmox_virtual_environment_sdn_applier.finalizer
+  ]
 }
 
 resource "proxmox_virtual_environment_sdn_zone_simple" "test_zone_2" {
   id = "tZone2"
-  nodes= [data.proxmox_virtual_environment_nodes.example.names]
+  nodes= data.proxmox_virtual_environment_nodes.example.names
   mtu = 1496
+  
+  depends_on = [
+    proxmox_virtual_environment_sdn_applier.finalizer
+  ]
 }
 
-resource "proxmox_virtual_environment_sdn_applier" "apply" {
+resource "proxmox_virtual_environment_sdn_applier" "applier" {
    lifecycle {
     replace_triggered_by = [
       proxmox_virtual_environment_sdn_zone_simple.test_zone_1,
@@ -22,4 +30,7 @@ resource "proxmox_virtual_environment_sdn_applier" "apply" {
     proxmox_virtual_environment_sdn_zone_simple.test_zone_1,
     proxmox_virtual_environment_sdn_zone_simple.test_zone_2,
   ]
+}
+
+resource "proxmox_virtual_environment_sdn_applier" "finalizer" {
 }
