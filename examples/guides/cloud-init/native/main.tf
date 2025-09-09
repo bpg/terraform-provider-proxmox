7483545c921e24bb7f57d1b9ec703d11bf2cdb3d
Pavel Boldyrev <627562+bpg@users.noreply.github.com>
2025-09-09 07:33:06 -0400

    chore(docs): improve `vm.initialization` documentation
    Signed-off-by: Pavel Boldyrev <627562+bpg@users.noreply.github.com>

--- examples/guides/cloud-init/native/main.tf
+++ examples/guides/cloud-init/native/main.tf
@@ -10,6 +10,9 @@ resource "proxmox_virtual_environment_vm" "ubuntu_vm" {
   stop_on_destroy = true
 
   initialization {
+    # uncomment and specify the datastore for cloud-init disk if default `local-lvm` is not available
+    # datastore_id = "local-lvm"
+
     ip_config {
       ipv4 {
         address = "192.168.3.233/24"
