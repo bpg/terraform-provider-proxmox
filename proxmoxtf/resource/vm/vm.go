d590df0b90a5a6aea5f5fefd2f4f5eb80e192b0f
Pavel Boldyrev <627562+bpg@users.noreply.github.com>
2025-09-13 10:07:04 -0400

    better boot device check
    Signed-off-by: Pavel Boldyrev <627562+bpg@users.noreply.github.com>

--- proxmoxtf/resource/vm/vm.go
+++ proxmoxtf/resource/vm/vm.go
@@ -5429,21 +5429,18 @@ func vmUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.D
 	// Handle disk deletion before applying other changes
 	if d.HasChange(disk.MkDisk) {
 		bootOrder := d.Get(mkBootOrder).([]interface{})
-		bootDevices := make([]string, len(bootOrder))
-
-		for i, device := range bootOrder {
-			bootDevices[i] = device.(string)
+		bootDeviceSet := make(map[string]struct{}, len(bootOrder))
+		for _, device := range bootOrder {
+			bootDeviceSet[device.(string)] = struct{}{}
 		}
 
 		for currentInterface := range allDiskInfo {
 			if _, present := planDisks[currentInterface]; !present {
-				for _, bootDevice := range bootDevices {
-					if bootDevice == currentInterface {
-						return diag.Errorf(
-							"cannot delete boot disk %q. Please remove it from boot order first or change boot order before deleting",
-							currentInterface,
-						)
-					}
+				if _, isBootDevice := bootDeviceSet[currentInterface]; isBootDevice {
+					return diag.Errorf(
+						"cannot delete boot disk %q. Please remove it from boot order first or change boot order before deleting",
+						currentInterface,
+					)
 				}
 
 				updateBody.Delete = append(updateBody.Delete, currentInterface)
