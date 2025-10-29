3397079e7a62e237e92ed1d623c462f69560cf9c
Stanislav Shamilov <shamilovstas@protonmail.com>
2025-10-29 14:00:32 +0200

    feat(provider): add acceptance tests for vm and storage pool memberships
    Signed-off-by: Stanislav Shamilov <shamilovstas@protonmail.com>

--- fwprovider/pools/pool_membership_test.go
+++ fwprovider/pools/pool_membership_test.go
@@ -4,13 +4,15 @@ import (
 	"context"
 	"fmt"
 	"math/rand"
+	"regexp"
 	"strconv"
 	"testing"
 	"time"
 
-	"github.com/bpg/terraform-provider-proxmox/fwprovider/test"
 	"github.com/bpg/terraform-provider-proxmox/proxmox/helpers/ptr"
 	"github.com/bpg/terraform-provider-proxmox/proxmox/nodes/storage"
+
+	"github.com/bpg/terraform-provider-proxmox/fwprovider/test"
 	"github.com/bpg/terraform-provider-proxmox/proxmox/pools"
 	"github.com/brianvoe/gofakeit/v7"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
@@ -27,11 +29,19 @@ func TestAccPoolMembershipContainer(t *testing.T) {
 	imageFileName := gofakeit.Word() + "-ubuntu-24.04-standard_24.04-2_amd64.tar.zst"
 	accTestContainerID := 100000 + rand.Intn(99999)
 	accTestContainerID2 := 100000 + rand.Intn(99999)
+	accTestPoolName := gofakeit.Word()
+
+	accTestPoolName2 := gofakeit.Word()
+	for accTestPoolName == accTestPoolName2 {
+		accTestPoolName2 = gofakeit.Word()
+	}
 
 	te.AddTemplateVars(map[string]interface{}{
 		"ImageFileName":    imageFileName,
 		"TestContainerID":  accTestContainerID,
 		"TestContainerID2": accTestContainerID2,
+		"TestPoolName":     accTestPoolName,
+		"TestPoolName2":    accTestPoolName2,
 	})
 
 	err := te.NodeStorageClient().DownloadFileByURL(context.Background(), &storage.DownloadURLPostRequestBody{
@@ -54,7 +64,7 @@ func TestAccPoolMembershipContainer(t *testing.T) {
 			{
 				Config: te.RenderConfig(`
 				resource "proxmox_virtual_environment_pool" "test_pool" {
-					pool_id = "test-pool"
+					pool_id = "{{ .TestPoolName }}"
 				}
 
 				resource "proxmox_virtual_environment_container" "test_container" {
@@ -76,41 +86,37 @@ func TestAccPoolMembershipContainer(t *testing.T) {
 					started = false
 				}
 				
-				resource "proxmox_virtual_environment_pool_membership" "test_ct_test_pool_membership" {
+				resource "proxmox_virtual_environment_pool_membership" "pool_membership" {
 					pool_id = proxmox_virtual_environment_pool.test_pool.id
 					vm_id = proxmox_virtual_environment_container.test_container.id
 				}`),
 				Check: resource.ComposeTestCheckFunc(
 					test.ResourceAttributes(
-						"proxmox_virtual_environment_pool_membership.test_ct_test_pool_membership",
+						"proxmox_virtual_environment_pool_membership.pool_membership",
 						map[string]string{
-							"pool_id": "test-pool",
+							"id":      fmt.Sprintf("%s/vm/%d", accTestPoolName, accTestContainerID),
+							"pool_id": accTestPoolName,
 							"vm_id":   strconv.Itoa(accTestContainerID),
+							"type":    "vm",
 						},
 					),
-					func(state *terraform.State) error {
-						ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-						defer cancel()
-
-						exists, poolCheckErr := testAccCheckPoolContainsMember(
-							ctx,
-							te.PoolsClient(),
-							"test-pool",
-							strconv.Itoa(accTestContainerID),
-							"lxc",
-						)
-						require.NoError(t, poolCheckErr, "couldn't get the pool")
-						require.True(t, exists)
-
-						return nil
-					},
+					testAccCheckPoolContainMember(t, te, accTestPoolName, strconv.Itoa(accTestContainerID), "lxc", true),
 				),
 			},
 			{
-				// destroy "test-pool" and create "test-pool-2"
+				ResourceName:      "proxmox_virtual_environment_pool_membership.pool_membership",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{
+				// change pool
 				Config: te.RenderConfig(`
+				resource "proxmox_virtual_environment_pool" "test_pool" {
+					pool_id = "{{ .TestPoolName }}"
+				}
+
 				resource "proxmox_virtual_environment_pool" "test_pool-2" {
-					pool_id = "test-pool-2"
+					pool_id = "{{ .TestPoolName2 }}"
 				}
 
 				resource "proxmox_virtual_environment_container" "test_container" {
@@ -132,23 +138,27 @@ func TestAccPoolMembershipContainer(t *testing.T) {
 					started = false
 				}
 				
-				resource "proxmox_virtual_environment_pool_membership" "test_ct_test_pool_membership" {
+				resource "proxmox_virtual_environment_pool_membership" "pool_membership" {
 					pool_id = proxmox_virtual_environment_pool.test_pool-2.id
 					vm_id = proxmox_virtual_environment_container.test_container.id
 				}`),
 				Check: resource.ComposeTestCheckFunc(
 					test.ResourceAttributes(
-						"proxmox_virtual_environment_pool_membership.test_ct_test_pool_membership",
+						"proxmox_virtual_environment_pool_membership.pool_membership",
 						map[string]string{
-							"pool_id": "test-pool-2",
+							"id":      fmt.Sprintf("%s/vm/%d", accTestPoolName2, accTestContainerID),
+							"pool_id": accTestPoolName2,
 							"vm_id":   strconv.Itoa(accTestContainerID),
+							"type":    "vm",
 						},
 					),
+					testAccCheckPoolContainMember(t, te, accTestPoolName, strconv.Itoa(accTestContainerID), "lxc", false),
+					testAccCheckPoolContainMember(t, te, accTestPoolName2, strconv.Itoa(accTestContainerID), "lxc", true),
 				),
 				ConfigPlanChecks: resource.ConfigPlanChecks{
 					PreApply: []plancheck.PlanCheck{
 						plancheck.ExpectResourceAction(
-							"proxmox_virtual_environment_pool_membership.test_ct_test_pool_membership",
+							"proxmox_virtual_environment_pool_membership.pool_membership",
 							plancheck.ResourceActionDestroyBeforeCreate,
 						),
 					},
@@ -158,7 +168,7 @@ func TestAccPoolMembershipContainer(t *testing.T) {
 				// destroy "test-container" and create "test-container-2"
 				Config: te.RenderConfig(`
 				resource "proxmox_virtual_environment_pool" "test_pool-2" {
-					pool_id = "test-pool-2"
+					pool_id = "{{ .TestPoolName2 }}"
 				}
 
 				resource "proxmox_virtual_environment_container" "test_container-2" {
@@ -180,23 +190,25 @@ func TestAccPoolMembershipContainer(t *testing.T) {
 					started = false
 				}
 				
-				resource "proxmox_virtual_environment_pool_membership" "test_ct_test_pool_membership" {
+				resource "proxmox_virtual_environment_pool_membership" "pool_membership" {
 					pool_id = proxmox_virtual_environment_pool.test_pool-2.id
 					vm_id = proxmox_virtual_environment_container.test_container-2.id
 				}`),
 				Check: resource.ComposeTestCheckFunc(
 					test.ResourceAttributes(
-						"proxmox_virtual_environment_pool_membership.test_ct_test_pool_membership",
+						"proxmox_virtual_environment_pool_membership.pool_membership",
 						map[string]string{
-							"pool_id": "test-pool-2",
+							"id":      fmt.Sprintf("%s/vm/%d", accTestPoolName2, accTestContainerID2),
+							"pool_id": accTestPoolName2,
 							"vm_id":   strconv.Itoa(accTestContainerID2),
+							"type":    "vm",
 						},
 					),
 				),
 				ConfigPlanChecks: resource.ConfigPlanChecks{
 					PreApply: []plancheck.PlanCheck{
 						plancheck.ExpectResourceAction(
-							"proxmox_virtual_environment_pool_membership.test_ct_test_pool_membership",
+							"proxmox_virtual_environment_pool_membership.pool_membership",
 							plancheck.ResourceActionDestroyBeforeCreate,
 						),
 					},
@@ -206,7 +218,7 @@ func TestAccPoolMembershipContainer(t *testing.T) {
 				// delete membership
 				Config: te.RenderConfig(`
 				resource "proxmox_virtual_environment_pool" "test_pool-2" {
-					pool_id = "test-pool-2"
+					pool_id = "{{ .TestPoolName2 }}"
 				}
 
 				resource "proxmox_virtual_environment_container" "test_container-2" {
@@ -227,30 +239,303 @@ func TestAccPoolMembershipContainer(t *testing.T) {
 					}
 					started = false
 				}`),
-				Check: resource.ComposeTestCheckFunc(
-					func(state *terraform.State) error {
-						ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-						defer cancel()
-
-						exists, poolCheckErr := testAccCheckPoolContainsMember(
-							ctx,
-							te.PoolsClient(),
-							"test-pool-2",
-							strconv.Itoa(accTestContainerID2),
-							"lxc",
-						)
-						require.NoError(t, poolCheckErr, "couldn't get the pool")
-						require.False(t, exists)
-
-						return nil
-					},
-				),
+				Check: testAccCheckPoolContainMember(t, te, accTestPoolName2, strconv.Itoa(accTestContainerID2), "lxc", false),
 			},
 		},
 	})
 }
 
-func testAccCheckPoolContainsMember(ctx context.Context, client *pools.Client, poolId, memberId, memberType string) (bool, error) {
+func TestAccPoolMembershipVm(t *testing.T) {
+	t.Parallel()
+
+	te := test.InitEnvironment(t)
+	accTestVmId := 100000 + rand.Intn(99999)
+	accTestVmId2 := 100000 + rand.Intn(99999)
+	accTestPoolName := gofakeit.Word()
+
+	accTestPoolName2 := gofakeit.Word()
+	for accTestPoolName == accTestPoolName2 {
+		accTestPoolName2 = gofakeit.Word()
+	}
+
+	te.AddTemplateVars(map[string]interface{}{
+		"TestVMID":      accTestVmId,
+		"TestVMID2":     accTestVmId2,
+		"TestPoolName":  accTestPoolName,
+		"TestPoolName2": accTestPoolName2,
+	})
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: te.AccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: te.RenderConfig(`
+				resource "proxmox_virtual_environment_vm" "test_vm1" {
+					vm_id = {{.TestVMID}}
+					node_name = "{{.NodeName}}"
+					started   = false
+				}
+				
+				resource "proxmox_virtual_environment_pool" "test_pool" {
+					pool_id = "{{.TestPoolName}}"
+				}
+				
+				resource "proxmox_virtual_environment_pool_membership" "pool_membership" {
+					pool_id = proxmox_virtual_environment_pool.test_pool.id
+					vm_id = proxmox_virtual_environment_vm.test_vm1.id
+				}`),
+				Check: resource.ComposeTestCheckFunc(
+					test.ResourceAttributes(
+						"proxmox_virtual_environment_pool_membership.pool_membership",
+						map[string]string{
+							"id":      fmt.Sprintf("%s/vm/%d", accTestPoolName, accTestVmId),
+							"pool_id": accTestPoolName,
+							"vm_id":   strconv.Itoa(accTestVmId),
+							"type":    "vm",
+						},
+					),
+					testAccCheckPoolContainMember(t, te, accTestPoolName, strconv.Itoa(accTestVmId), "qemu", true),
+				),
+			},
+			{
+				ResourceName:      "proxmox_virtual_environment_pool_membership.pool_membership",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{ // change pool
+				Config: te.RenderConfig(`
+				resource "proxmox_virtual_environment_pool" "test_pool" {
+					pool_id = "{{ .TestPoolName }}"
+				}
+
+				resource "proxmox_virtual_environment_pool" "test_pool-2" {
+					pool_id = "{{ .TestPoolName2 }}"
+				}
+
+				resource "proxmox_virtual_environment_vm" "test_vm1" {
+					vm_id = {{.TestVMID}}
+					node_name = "{{.NodeName}}"
+					started   = false
+				}
+				
+				resource "proxmox_virtual_environment_pool_membership" "pool_membership" {
+					pool_id = proxmox_virtual_environment_pool.test_pool-2.id
+					vm_id = proxmox_virtual_environment_vm.test_vm1.id
+				}`),
+				Check: resource.ComposeTestCheckFunc(
+					test.ResourceAttributes(
+						"proxmox_virtual_environment_pool_membership.pool_membership",
+						map[string]string{
+							"id":      fmt.Sprintf("%s/vm/%d", accTestPoolName2, accTestVmId),
+							"pool_id": accTestPoolName2,
+							"vm_id":   strconv.Itoa(accTestVmId),
+							"type":    "vm",
+						},
+					),
+					testAccCheckPoolContainMember(t, te, accTestPoolName, strconv.Itoa(accTestVmId), "qemu", false),
+					testAccCheckPoolContainMember(t, te, accTestPoolName2, strconv.Itoa(accTestVmId), "qemu", true),
+				),
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(
+							"proxmox_virtual_environment_pool_membership.pool_membership",
+							plancheck.ResourceActionDestroyBeforeCreate,
+						),
+					},
+				},
+			},
+			{ // destroy "test-vm1" and create "test-vm2"
+				Config: te.RenderConfig(`
+				resource "proxmox_virtual_environment_pool" "test_pool-2" {
+					pool_id = "{{ .TestPoolName2 }}"
+				}
+
+				resource "proxmox_virtual_environment_vm" "test_vm2" {
+					vm_id = {{.TestVMID2}}
+					node_name = "{{.NodeName}}"
+					started   = false
+				}
+				
+				resource "proxmox_virtual_environment_pool_membership" "pool_membership" {
+					pool_id = proxmox_virtual_environment_pool.test_pool-2.id
+					vm_id = proxmox_virtual_environment_vm.test_vm2.id
+				}`),
+				Check: resource.ComposeTestCheckFunc(
+					test.ResourceAttributes(
+						"proxmox_virtual_environment_pool_membership.pool_membership",
+						map[string]string{
+							"id":      fmt.Sprintf("%s/vm/%d", accTestPoolName2, accTestVmId2),
+							"pool_id": accTestPoolName2,
+							"vm_id":   strconv.Itoa(accTestVmId2),
+							"type":    "vm",
+						},
+					),
+				),
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(
+							"proxmox_virtual_environment_pool_membership.pool_membership",
+							plancheck.ResourceActionDestroyBeforeCreate,
+						),
+					},
+				},
+			},
+			{ // delete membership
+				Config: te.RenderConfig(`
+				resource "proxmox_virtual_environment_pool" "test_pool-2" {
+					pool_id = "{{ .TestPoolName2 }}"
+				}
+
+				resource "proxmox_virtual_environment_vm" "test_vm2" {
+					vm_id = {{.TestVMID2}}
+					node_name = "{{.NodeName}}"
+					started   = false
+				}`),
+				Check: testAccCheckPoolContainMember(t, te, accTestPoolName2, strconv.Itoa(accTestVmId2), "qemu", false),
+			},
+		},
+	})
+}
+
+func TestAccPoolMembershipStorage(t *testing.T) {
+	t.Parallel()
+
+	te := test.InitEnvironment(t)
+	accTestPoolName := gofakeit.Word()
+
+	accTestPoolName2 := gofakeit.Word()
+	for accTestPoolName == accTestPoolName2 {
+		accTestPoolName2 = gofakeit.Word()
+	}
+
+	te.AddTemplateVars(map[string]interface{}{
+		"TestPoolName":  accTestPoolName,
+		"TestPoolName2": accTestPoolName2,
+	})
+
+	resource.Test(t, resource.TestCase{
+		ProtoV6ProviderFactories: te.AccProviders,
+		Steps: []resource.TestStep{
+			{
+				Config: te.RenderConfig(`
+				resource "proxmox_virtual_environment_pool" "test_pool" {
+					pool_id = "{{.TestPoolName}}"
+				}
+				
+				resource "proxmox_virtual_environment_pool_membership" "pool_membership" {
+					pool_id = proxmox_virtual_environment_pool.test_pool.id
+					storage_id = "{{ .DatastoreID }}"
+				}`),
+				Check: resource.ComposeTestCheckFunc(
+					test.ResourceAttributes(
+						"proxmox_virtual_environment_pool_membership.pool_membership",
+						map[string]string{
+							"id":         fmt.Sprintf("%s/storage/%s", accTestPoolName, "local"),
+							"pool_id":    accTestPoolName,
+							"storage_id": "local",
+							"type":       "storage",
+						},
+					),
+					testAccCheckPoolContainMember(t, te, accTestPoolName, "local", "storage", true),
+				),
+			},
+			{
+				ResourceName:      "proxmox_virtual_environment_pool_membership.pool_membership",
+				ImportState:       true,
+				ImportStateVerify: true,
+			},
+			{ // change pool
+				Config: te.RenderConfig(`
+				resource "proxmox_virtual_environment_pool" "test_pool" {
+					pool_id = "{{ .TestPoolName }}"
+				}
+				resource "proxmox_virtual_environment_pool" "test_pool-2" {
+					pool_id = "{{ .TestPoolName2 }}"
+				}
+				
+				resource "proxmox_virtual_environment_pool_membership" "pool_membership" {
+					pool_id = proxmox_virtual_environment_pool.test_pool-2.id
+					storage_id = "{{ .DatastoreID }}"
+				}`),
+				Check: resource.ComposeTestCheckFunc(
+					test.ResourceAttributes(
+						"proxmox_virtual_environment_pool_membership.pool_membership",
+						map[string]string{
+							"id":         fmt.Sprintf("%s/storage/%s", accTestPoolName2, "local"),
+							"pool_id":    accTestPoolName2,
+							"storage_id": "local",
+							"type":       "storage",
+						},
+					),
+					testAccCheckPoolContainMember(t, te, accTestPoolName2, "local", "storage", true),
+					testAccCheckPoolContainMember(t, te, accTestPoolName, "local", "storage", false),
+				),
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(
+							"proxmox_virtual_environment_pool_membership.pool_membership",
+							plancheck.ResourceActionDestroyBeforeCreate,
+						),
+					},
+				},
+			},
+			{ // delete membership
+				Config: te.RenderConfig(`
+				resource "proxmox_virtual_environment_pool" "test_pool-2" {
+					pool_id = "{{ .TestPoolName2 }}"
+				}`),
+				Check: testAccCheckPoolContainMember(t, te, accTestPoolName2, "local", "storage", false),
+			},
+		},
+	})
+}
+
+func TestAccPoolMembership_Validators(t *testing.T) {
+	t.Parallel()
+
+	te := test.InitEnvironment(t)
+
+	resource.UnitTest(t, resource.TestCase{
+		ProtoV6ProviderFactories: te.AccProviders,
+		CheckDestroy:             nil,
+		Steps: []resource.TestStep{
+			{
+				PlanOnly: true,
+				Config: `resource "proxmox_virtual_environment_pool_membership" "test" {
+					pool_id = "test_pool"
+					storage_id = "local"
+					vm_id = 1234
+				}`,
+				ExpectError: regexp.MustCompile(`.*Error: Invalid Attribute Combination`),
+			},
+			{
+				PlanOnly: true,
+				Config: `resource "proxmox_virtual_environment_pool_membership" "test" {
+					pool_id = "test_pool"
+				}`,
+				ExpectError: regexp.MustCompile(`.*Error: Missing Attribute Configuration`),
+			},
+			{
+				PlanOnly: true,
+				Config: `resource "proxmox_virtual_environment_pool_membership" "test" {
+					pool_id = "test_pool"
+					vm_id = 1234
+				}`,
+				ExpectNonEmptyPlan: true,
+			},
+			{
+				PlanOnly: true,
+				Config: `resource "proxmox_virtual_environment_pool_membership" "test" {
+					pool_id = "test_pool"
+					storage_id = "local"
+				}`,
+				ExpectNonEmptyPlan: true,
+			},
+		},
+	})
+}
+
+func checkPoolContainsMember(ctx context.Context, client *pools.Client, poolId, memberId, memberType string) (bool, error) {
 	pool, err := client.GetPool(ctx, poolId)
 	if err != nil {
 		return false, err
@@ -274,4 +559,25 @@ func testAccCheckPoolContainsMember(ctx context.Context, client *pools.Client, p
 	}
 
 	return exists, err
+}
+
+func testAccCheckPoolContainMember(t *testing.T, te *test.Environment, poolName, memberId, memberType string, shouldExist bool) resource.TestCheckFunc {
+	t.Helper()
+
+	return func(state *terraform.State) error {
+		ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+		defer cancel()
+
+		exists, poolCheckErr := checkPoolContainsMember(
+			ctx,
+			te.PoolsClient(),
+			poolName,
+			memberId,
+			memberType,
+		)
+		require.NoError(t, poolCheckErr, "couldn't get the pool")
+		require.Equal(t, shouldExist, exists)
+
+		return nil
+	}
 }
