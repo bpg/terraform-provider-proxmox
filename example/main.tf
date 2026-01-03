f3ee89cab07d722a55661ed781a33ad07c8f630a
Daniel Velasquez Neira <42185968+dannyvelas@users.noreply.github.com>
2026-01-03 16:27:00 -0500

    fix(docs): make example use password auth so lxc bind works
    Signed-off-by: Daniel Velasquez Neira <42185968+dannyvelas@users.noreply.github.com>

--- example/main.tf
+++ example/main.tf
@@ -1,9 +1,6 @@
 provider "proxmox" {
-  endpoint  = var.virtual_environment_endpoint
-  api_token = var.virtual_environment_api_token
-  insecure  = true
-  ssh {
-    agent    = true
-    username = var.virtual_environment_ssh_username
-  }
+  endpoint = var.virtual_environment_endpoint
+  insecure = true
+  username = var.virtual_environment_username
+  password = var.virtual_environment_password
 }
