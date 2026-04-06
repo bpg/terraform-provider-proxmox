b6ae42edd9ab353b16a72efce2a210148be3b25e
Pavel Boldyrev <pavel@bpg.sh>
2026-04-06 07:00:20 -0400

    add acceptance test & fix a backward compatibility issue.
    Signed-off-by: Pavel Boldyrev <pavel@bpg.sh>

--- proxmoxtf/resource/container/container.go
+++ proxmoxtf/resource/container/container.go
@@ -238,11 +238,19 @@ func Container() *schema.Resource {
 							Default:     dvCloneNodeName,
 						},
 						mkCloneFull: {
-							Type:        schema.TypeBool,
-							Description: "When cloning, create a full copy of all disks. Set to false to create a linked clone.",
-							Optional:    true,
-							ForceNew:    true,
-							Default:     dvCloneFull,
+							Type: schema.TypeBool,
+							Description: "When cloning, create a full copy of all disks. Set to false to create a linked " +
+								"clone. Linked clones require the source container to be a template on storage that supports " +
+								"copy-on-write (e.g. Ceph RBD). Defaults to true.",
+							Optional: true,
+							ForceNew: true,
+							Default:  dvCloneFull,
+							DiffSuppressFunc: func(_, oldVal, newVal string, d *schema.ResourceData) bool {
+								// Suppress diff when upgrading from a provider version that didn't have this attribute.
+								// In that case oldVal is "" (missing from state) and newVal is "true" (from Default).
+								// Only suppress for existing resources (d.Id() != ""), not during creation.
+								return d.Id() != "" && oldVal == "" && newVal == "true"
+							},
 						},
 						mkCloneVMID: {
 							Type:             schema.TypeInt,
