d66f40365ad66f514482e85cc97ba9a1e18cddb7
hurack <hurack3034217@gmail.com>
2026-04-30 13:06:55 +0000

    feat(sdn): add acceptance test
    Signed-off-by: hurack <hurack3034217@gmail.com>

---
+++ fwprovider/test/resource_sdn_controller_test.go
@@ -0,0 +1,92 @@
+//go:build acceptance || all
+
+//testacc:tier=heavy
+//testacc:resource=sdn
+
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package test
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+func TestAccResourceSDNControllerEVPN(t *testing.T) {
+	// Cannot run in parallel due to SDN applier functionality affecting global state
+
+	te := InitEnvironment(t)
+
+	tests := []struct {
+		name  string
+		steps []resource.TestStep
+	}{
+		{"create and update controller", []resource.TestStep{{
+			Config: te.RenderConfig(`
+				resource "proxmox_sdn_controller_evpn" "controller_evpn" {
+				  id  = "ctrlE"
+				  peers = ["10.0.0.1"]
+				  asn = 65000
+				  depends_on = [
+				    proxmox_sdn_applier.finalizer
+				  ]
+				}
+
+				resource "proxmox_sdn_applier" "main" {
+				  depends_on = [
+				    proxmox_sdn_controller_evpn.controller_evpn
+				  ]
+				}
+				resource "proxmox_sdn_applier" "finalizer" {}
+			`),
+			Check: resource.ComposeTestCheckFunc(
+				ResourceAttributes("proxmox_sdn_controller_evpn.controller_evpn", map[string]string{
+					"id":      "ctrlE",
+					"peers.#": "1",
+					"peers.0": "10.0.0.1",
+					"asn":     "65000",
+				}),
+				NoResourceAttributesSet("proxmox_sdn_controller_evpn.controller_evpn", []string{
+					"fabric",
+				}),
+			),
+		}, {
+			Config: te.RenderConfig(`
+				resource "proxmox_sdn_controller_evpn" "controller_evpn" {
+				  id  = "ctrlE"
+				  peers = ["10.0.0.2"]
+				  asn = 65000
+				}
+			`),
+			Check: resource.ComposeTestCheckFunc(
+				ResourceAttributes("proxmox_sdn_controller_evpn.controller_evpn", map[string]string{
+					"id":      "ctrlE",
+					"peers.#": "1",
+					"peers.0": "10.0.0.2",
+					"asn":     "65000",
+				}),
+				NoResourceAttributesSet("proxmox_sdn_controller_evpn.controller_evpn", []string{
+					"fabric",
+				}),
+			),
+			ResourceName:      "proxmox_sdn_controller_evpn.controller_evpn",
+			ImportStateId:     "ctrlE",
+			ImportState:       true,
+			ImportStateVerify: true,
+		}}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			resource.Test(t, resource.TestCase{
+				ProtoV6ProviderFactories: te.AccProviders,
+				Steps:                    tt.steps,
+			})
+		})
+	}
+}
