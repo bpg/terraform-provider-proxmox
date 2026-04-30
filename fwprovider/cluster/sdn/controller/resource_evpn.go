875419d833d9ac85bd2a7f109a37bd4372785b58
hurack <hurack3034217@gmail.com>
2026-04-30 13:06:33 +0000

    feat(sdn): add EVPN controller resources
    Signed-off-by: hurack <hurack3034217@gmail.com>

---
+++ fwprovider/cluster/sdn/controller/resource_evpn.go
@@ -0,0 +1,176 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package controller
+
+import (
+	"context"
+	"fmt"
+	"strings"
+
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+
+	"github.com/bpg/terraform-provider-proxmox/fwprovider/types/stringset"
+	"github.com/bpg/terraform-provider-proxmox/proxmox/cluster/sdn/controllers"
+	proxmoxtypes "github.com/bpg/terraform-provider-proxmox/proxmox/types"
+)
+
+var (
+	_ resource.ResourceWithConfigure   = &EVPNResource{}
+	_ resource.ResourceWithImportState = &EVPNResource{}
+)
+
+type evpnModel struct {
+	genericModel
+
+	ASNumber types.Int64     `tfsdk:"asn"`
+	FabricID types.String    `tfsdk:"fabric"`
+	Peers    stringset.Value `tfsdk:"peers"`
+}
+
+func (m *evpnModel) fromAPI(name string, data *controllers.ControllerData, diags *diag.Diagnostics) {
+	m.genericModel.fromAPI(name, data, diags)
+
+	m.ASNumber = types.Int64PointerValue(data.ASNumber)
+	m.FabricID = types.StringPointerValue(data.Fabric)
+
+	if data.Peers != nil {
+		peers := make([]string, len(*data.Peers))
+		copy(peers, *data.Peers)
+		m.Peers = stringset.NewValueList(peers, diags)
+	} else {
+		m.Peers = stringset.NullValue()
+	}
+}
+
+func (m *evpnModel) toAPI(ctx context.Context, diags *diag.Diagnostics) *controllers.Controller {
+	data := m.genericModel.toAPI(ctx, diags)
+
+	data.ASNumber = m.ASNumber.ValueInt64Pointer()
+	data.Fabric = m.FabricID.ValueStringPointer()
+
+	var peers []string
+	diags.Append(m.Peers.ElementsAs(ctx, &peers, false)...)
+
+	if len(m.Peers.Elements()) > 0 {
+		for i, peer := range m.Peers.Elements() {
+			peers[i] = strings.TrimSpace(peer.String())
+			peers[i] = strings.Trim(peers[i], `"`)
+		}
+
+		data.Peers = new((proxmoxtypes.CustomCommaSeparatedList)(peers))
+	} else {
+		data.Peers = nil
+	}
+
+	return data
+}
+
+func checkDeletedEVPNFields(plan, state *evpnModel) []string {
+	var toDelete []string
+
+	if plan.ASNumber.IsNull() && !state.ASNumber.IsNull() {
+		toDelete = append(toDelete, "asn")
+	}
+
+	if plan.FabricID.IsNull() && !state.FabricID.IsNull() {
+		toDelete = append(toDelete, "fabric")
+	}
+
+	if plan.Peers.IsNull() && !state.Peers.IsNull() {
+		toDelete = append(toDelete, "peers")
+	}
+
+	return toDelete
+}
+
+type EVPNResource struct {
+	*genericControllerResource
+}
+
+func NewEVPNResource() resource.Resource {
+	return &EVPNResource{
+		genericControllerResource: newGenericControllerResource(controllerResourceConfig{
+			typeNameSuffix: "_sdn_controller_evpn",
+			controllerType: controllers.TypeEVPN,
+			modelFunc:      func() controllerModel { return &evpnModel{} },
+		}).(*genericControllerResource),
+	}
+}
+
+func (r *EVPNResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	var plan evpnModel
+	resp.Diagnostics.Append(req.Plan.Get(ctx, &plan)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	var state evpnModel
+	resp.Diagnostics.Append(req.State.Get(ctx, &state)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	toDelete := checkDeletedEVPNFields(&plan, &state)
+
+	updateEVPNController := plan.toAPI(ctx, &resp.Diagnostics)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	update := &controllers.ControllerUpdate{
+		Controller: *updateEVPNController,
+		Delete:     toDelete,
+	}
+
+	err := r.client.UpdateController(ctx, update)
+	if err != nil {
+		resp.Diagnostics.AddError(
+			"Error Updating EVPN SDN Controller",
+			fmt.Sprintf("Could not update EVPN SDN Controller %q: %v", plan.ID.ValueString(), err),
+		)
+
+		return
+	}
+
+	r.readAndSetState(ctx, plan.getID(), &resp.State, &resp.Diagnostics)
+}
+
+func (r *EVPNResource) Schema(_ context.Context, _ resource.SchemaRequest, resp *resource.SchemaResponse) {
+	resp.Schema = schema.Schema{
+		Description: "The EVPN controller plugin configures the Free Range Routing (frr) router.",
+		MarkdownDescription: "The EVPN, zone requires an external controller to manage the control plane." +
+			" The EVPN controller plugin configures the Free Range Routing (frr) router.",
+		Attributes: genericAttributesWith(map[string]schema.Attribute{
+			"asn": schema.Int64Attribute{
+				Description: "Autonomous System Number for the EVPN controller.",
+				Required:    true,
+			},
+			"fabric": schema.StringAttribute{
+				Description: "ID of the fabric this EVPN controller belongs to.",
+				Optional:    true,
+			},
+			"peers": schema.SetAttribute{
+				CustomType: stringset.Type{
+					SetType: types.SetType{
+						ElemType: types.StringType,
+					},
+				},
+				Description: "Set of BGP peer IP addresses for the EVPN controller.",
+				Optional:    true,
+			},
+		}),
+	}
+}
+
+func (m *evpnModel) getGenericModel() *genericModel {
+	return &m.genericModel
+}
---
+++ fwprovider/cluster/sdn/controller/resource_generic.go
@@ -0,0 +1,276 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+package controller
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"maps"
+
+	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/resource"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/tfsdk"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+
+	"github.com/bpg/terraform-provider-proxmox/fwprovider/config"
+	"github.com/bpg/terraform-provider-proxmox/fwprovider/validators"
+	"github.com/bpg/terraform-provider-proxmox/proxmox/api"
+	"github.com/bpg/terraform-provider-proxmox/proxmox/cluster/sdn"
+	"github.com/bpg/terraform-provider-proxmox/proxmox/cluster/sdn/controllers"
+	proxmoxtypes "github.com/bpg/terraform-provider-proxmox/proxmox/types"
+)
+
+type genericModel struct {
+	ID     types.String `tfsdk:"id"`
+	Digest types.String `tfsdk:"digest"`
+	Type   types.String `tfsdk:"-"`
+}
+
+func (m *genericModel) fromAPI(name string, data *controllers.ControllerData, _ *diag.Diagnostics) {
+	m.ID = types.StringValue(name)
+	m.Type = types.StringPointerValue(data.Type)
+
+	m.Digest = m.handleDeletedStringValue(data.Digest)
+}
+
+func (m *genericModel) toAPI(_ context.Context, _ *diag.Diagnostics) *controllers.Controller {
+	data := &controllers.Controller{}
+
+	data.ID = m.ID.ValueString()
+	data.Type = m.Type.ValueStringPointer()
+
+	return data
+}
+
+func (m *genericModel) handleDeletedStringValue(value *string) types.String {
+	if value == nil {
+		return types.StringNull()
+	}
+
+	return types.StringValue(*value)
+}
+
+func checkDeletedFields(_, _ *genericModel) []string {
+	var toDelete []string
+
+	return toDelete
+}
+
+func (m *genericModel) getID() string {
+	return m.ID.ValueString()
+}
+
+func genericAttributesWith(extraAttributes map[string]schema.Attribute) map[string]schema.Attribute {
+	// Start with generic attributes as the base
+	result := map[string]schema.Attribute{
+		"id": schema.StringAttribute{
+			Description: "The SDN controller object identifier.",
+			Required:    true,
+			PlanModifiers: []planmodifier.String{
+				stringplanmodifier.RequiresReplace(),
+			},
+			Validators: validators.SDNID(),
+		},
+		"digest": schema.StringAttribute{
+			Description: "Digest of the controller section.",
+			Computed:    true,
+		},
+	}
+
+	// Add extra attributes, allowing them to override generic ones if needed
+	if extraAttributes != nil {
+		maps.Copy(result, extraAttributes)
+	}
+
+	return result
+}
+
+type controllerModel interface {
+	fromAPI(name string, data *controllers.ControllerData, diags *diag.Diagnostics)
+	toAPI(ctx context.Context, diags *diag.Diagnostics) *controllers.Controller
+	getID() string
+	getGenericModel() *genericModel
+}
+
+type controllerResourceConfig struct {
+	typeNameSuffix string
+	controllerType string
+	modelFunc      func() controllerModel
+}
+
+type genericControllerResource struct {
+	client *controllers.Client
+	config controllerResourceConfig
+}
+
+func newGenericControllerResource(cfg controllerResourceConfig) resource.Resource {
+	return &genericControllerResource{config: cfg}
+}
+
+func (r *genericControllerResource) Metadata(_ context.Context, req resource.MetadataRequest, resp *resource.MetadataResponse) {
+	resp.TypeName = "proxmox" + r.config.typeNameSuffix
+}
+
+func (r *genericControllerResource) Configure(_ context.Context, req resource.ConfigureRequest, resp *resource.ConfigureResponse) {
+	if req.ProviderData == nil {
+		return
+	}
+
+	cfg, ok := req.ProviderData.(config.Resource)
+	if !ok {
+		resp.Diagnostics.AddError(
+			"Unexpected Resource Configure Type",
+			fmt.Sprintf("Expected config.Resource, got: %T", req.ProviderData),
+		)
+
+		return
+	}
+
+	r.client = cfg.Client.Cluster().SDNControllers()
+}
+
+func (r *genericControllerResource) Create(ctx context.Context, req resource.CreateRequest, resp *resource.CreateResponse) {
+	plan := r.config.modelFunc()
+	resp.Diagnostics.Append(req.Plan.Get(ctx, plan)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	newController := plan.toAPI(ctx, &resp.Diagnostics)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	newController.Type = new(r.config.controllerType)
+
+	if err := r.client.CreateController(ctx, newController); err != nil {
+		resp.Diagnostics.AddError("Unable to Create SDN Controller", err.Error())
+		return
+	}
+
+	r.readAndSetState(ctx, plan.getID(), &resp.State, &resp.Diagnostics)
+}
+
+func (r *genericControllerResource) Read(ctx context.Context, req resource.ReadRequest, resp *resource.ReadResponse) {
+	state := r.config.modelFunc()
+	resp.Diagnostics.Append(req.State.Get(ctx, state)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	controller, err := r.client.GetController(ctx, state.getID())
+	if err != nil {
+		if errors.Is(err, api.ErrResourceDoesNotExist) {
+			resp.State.RemoveResource(ctx)
+			return
+		}
+
+		resp.Diagnostics.AddError("Unable to Read SDN Controller", err.Error())
+
+		return
+	}
+
+	r.setModelFromController(ctx, controller, &resp.State, &resp.Diagnostics)
+}
+
+func (r *genericControllerResource) Update(ctx context.Context, req resource.UpdateRequest, resp *resource.UpdateResponse) {
+	plan := r.config.modelFunc()
+	resp.Diagnostics.Append(req.Plan.Get(ctx, plan)...)
+
+	state := r.config.modelFunc()
+	resp.Diagnostics.Append(req.State.Get(ctx, state)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	updateController := plan.toAPI(ctx, &resp.Diagnostics)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	toDelete := checkDeletedFields(plan.getGenericModel(), state.getGenericModel())
+	update := &controllers.ControllerUpdate{
+		Controller: *updateController,
+		Delete:     toDelete,
+	}
+
+	if err := r.client.UpdateController(ctx, update); err != nil {
+		resp.Diagnostics.AddError("Unable to Update SDN Controller", err.Error())
+
+		return
+	}
+
+	r.readAndSetState(ctx, plan.getID(), &resp.State, &resp.Diagnostics)
+}
+
+func (r *genericControllerResource) Delete(ctx context.Context, req resource.DeleteRequest, resp *resource.DeleteResponse) {
+	state := r.config.modelFunc()
+	resp.Diagnostics.Append(req.State.Get(ctx, state)...)
+
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	if err := r.client.DeleteController(ctx, state.getID()); err != nil &&
+		!errors.Is(err, api.ErrResourceDoesNotExist) {
+		resp.Diagnostics.AddError("Unable to Delete SDN Controller", err.Error())
+	}
+}
+
+func (r *genericControllerResource) ImportState(ctx context.Context, req resource.ImportStateRequest, resp *resource.ImportStateResponse) {
+	controller, err := r.client.GetControllerWithParams(ctx, req.ID, &sdn.QueryParams{Pending: proxmoxtypes.CustomBool(true).Pointer()})
+	if err != nil {
+		if errors.Is(err, api.ErrResourceDoesNotExist) {
+			resp.Diagnostics.AddError(fmt.Sprintf("Controller %s does not exist", req.ID), err.Error())
+			return
+		}
+
+		resp.Diagnostics.AddError(fmt.Sprintf("Unable to Import SDN Controller %s", req.ID), err.Error())
+
+		return
+	}
+
+	r.setModelFromController(ctx, controller, &resp.State, &resp.Diagnostics)
+}
+
+// Schema is required to satisfy the resource.Resource interface. It should be implemented by the specific resource.
+func (r *genericControllerResource) Schema(_ context.Context, _ resource.SchemaRequest, _ *resource.SchemaResponse) {
+	// Intentionally left blank. Should be set by the specific resource.
+}
+
+func (r *genericControllerResource) readAndSetState(ctx context.Context, controllerID string, state *tfsdk.State, diags *diag.Diagnostics) {
+	controller, err := r.client.GetController(ctx, controllerID)
+	if err != nil {
+		diags.AddError("Unable to Read SDN Controller", err.Error())
+		return
+	}
+
+	r.setModelFromController(ctx, controller, state, diags)
+}
+
+func (r *genericControllerResource) setModelFromController(
+	ctx context.Context,
+	controller *controllers.ControllerData,
+	state *tfsdk.State,
+	diags *diag.Diagnostics,
+) {
+	readModel := r.config.modelFunc()
+	readModel.fromAPI(controller.ID, controller, diags)
+
+	if diags.HasError() {
+		return
+	}
+
+	diags.Append(state.Set(ctx, readModel)...)
+}
